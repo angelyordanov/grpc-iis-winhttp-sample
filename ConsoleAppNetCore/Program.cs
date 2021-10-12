5cafd895e9c7a1b494f445821a5dbc010369e02f
Angel Yordanov <yordanov@gmail.com>
2021-10-12 16:18:32 +0300

    Initial commit

---
+++ ConsoleAppNetCore/Program.cs
@@ -0,0 +1,59 @@
+﻿using Grpc.Net.ClientFactory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using static GrpcService.Greeter;
+
+namespace ConsoleAppNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services
+                .AddGrpcClient<GreeterClient>(
+                    "WinHttp",
+                    (options) =>
+                    {
+                        options.Address = new Uri("https://localhost:44323/");
+                    })
+                .ConfigurePrimaryHttpMessageHandler(
+                    () => new WinHttpHandler()
+                    {
+                        EnableMultipleHttp2Connections = true
+                    });
+            services
+                .AddGrpcClient<GreeterClient>(
+                    "Sockets",
+                    (options) =>
+                    {
+                        options.Address = new Uri("https://localhost:44323/");
+                    });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var factory = serviceProvider.GetRequiredService<GrpcClientFactory>();
+            var winHttpClient = factory.CreateClient<GreeterClient>("WinHttp");
+            var socketsClient = factory.CreateClient<GreeterClient>("Sockets");
+
+            var resp1 = socketsClient.SayHello(
+                new GrpcService.HelloRequest
+                {
+                    Name = "ConsoleAppNetCore"
+                });
+            try
+            {
+                var resp2 = winHttpClient.SayHello(
+                    new GrpcService.HelloRequest
+                    {
+                        Name = "ConsoleAppNetCore"
+                    });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleAppNetFramework/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using static GrpcService.Greeter;
+
+namespace ConsoleAppNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services
+                .AddGrpcClient<GreeterClient>(
+                    (options) =>
+                    {
+                        options.Address = new Uri("https://localhost:44323/");
+                    })
+                .ConfigurePrimaryHttpMessageHandler(
+                    () => new WinHttpHandler()
+                    {
+                        EnableMultipleHttp2Connections = true
+                    });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var client = serviceProvider.GetRequiredService<GreeterClient>();
+            var resp1 = client.SayHello(
+                new GrpcService.HelloRequest
+                {
+                    Name = "ConsoleAppNetFramework"
+                });
+            try
+            {
+                var resp2 = client.SayHello(
+                    new GrpcService.HelloRequest
+                    {
+                        Name = "ConsoleAppNetFramework"
+                    });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
